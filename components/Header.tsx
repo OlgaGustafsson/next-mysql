61d31cd8b1a74a8b1a33dd8cda702d89a0c5d0ab
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2023-10-27 13:09:36 +0200

    update styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,14 @@ import Navbar from './Navbar'
 export default function Header() {
   return (
     <header className="sticky top-0 flex flex-wrap items-center 
-    justify-around h-24 bg-darkgray text-teal-900">
+    justify-around h-24 bg-teal-900 text-teal-900">
         <Link href="/">
           <div className="flex flex-wrap justify-center text-yellow-600">
             <h1 className="text-4xl">DOC.create</h1>
           </div>
         </Link>
 
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center  text-gray-400">
           
           <Navbar />
 
